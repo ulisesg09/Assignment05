6a8f4d93568179b13c4ac32f8680eeba63bff66c
ulisesg09 <ulisesulises3@gmail.com>
2017-05-03 12:46:46 -0700

    first commit

---
+++ Code/app.js
@@ -0,0 +1,27 @@
+var express = require('express');
+
+var app = express();
+
+var port = process. env.PORT || 3000;
+
+var bookRouter = express.Router();
+
+bookRouter.route('/Books')
+    .get(function(req,res){
+    var responseJson = {hello: "This is my api"};
+    
+        res.json(responseJson);
+});
+
+app.use('/api', bookRouter);
+
+app.get('/', function(req, res){
+    res.send('welcome to my API');
+});
+
+app.listen(port,function(){
+    console.log('Gulp is running my app on PORT: ' + port)
+});
+
+
+
